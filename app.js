2a7f2402e4dbd927c4bd064f080be975384f4dfc
RomaneGustin24 <raugustin1049@gmail.com>
2019-04-11 11:06:02 -0400

    just added the index file for routes

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
-var express = require('express'),
-    app     = express(),
-    ejs     = require('ejs'),
-    mongoose= require('mongoose'),
-    bodyParser= require('body-parser');
+var express     =   require("express"),
+app         =   express(),
+bodyParser  =   require("body-parser"),
+mongoose    =   require("mongoose"),
+passport    =   require("passport"),
+LocalStrategy = require("passport-local"),
+methodOverride = require("method-override"),
+Fake         = require("faker"),
+seedDB       = require("./seeds"),
+Load         = require("./models/load"),
+Truck         = require("./models/truck"),
+User        =   require("./models/loginUser");
 
-  
+//requiring routes
+var indexRoutes = require("./routes/index"),
+    carrierRoutes = require("./routes/carrier"),
+    brokerRoutes = require("./routes/broker");
+//have to add for trucks, companies, loads, etc..
+
+mongoose.connect("mongodb://localhost/autmov-3");
+ 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+//seedDB(); //seed the database
+
+
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+ });
+
+
+app.use(require("express-session")({
+    secret: "!t7is%,will$4tre#(if-@b+yral9",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//dictate the route paths
+app.use("/", indexRoutes);
+app.use("/carrier", carrierRoutes);
+app.use("/broker", brokerRoutes);
+
+app.listen("3000", "localhost", ()=> {
+    console.log("aulomov app has started successfully");
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+    router  = express.Router(),
+    passport = require("passport");
+
+//models
+var User = require("../models/User");
+
+ 
+
+module.exports = router;
+
+
